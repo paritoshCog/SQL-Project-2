0ce309a4bdd831f7612ea6c3c59aa776dd426e01
Paritosh Prasad <128465174+paritoshCog@users.noreply.github.com>
2023-04-10 12:36:35 +0530

    Update northwind.sql

--- northwind.sql
+++ northwind.sql
@@ -16,3 +16,9 @@ group by
 -- Show the city, company_name, contact_name from the customers and suppliers table merged together.
 -- Create a column which contains 'customers' or 'suppliers' depending on the table it came from.
 
+select City, company_name, contact_name, 'customers' as relationship 
+from customers
+union
+select city, company_name, contact_name, 'suppliers'
+from suppliers
+
